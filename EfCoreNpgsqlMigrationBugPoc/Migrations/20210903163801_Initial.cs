e3b0a9f903d27a5854a3b8f517f7391057b969f8
Oleg Kosmakov <1533952+kosmakoff@users.noreply.github.com>
2021-09-03 18:38:24 +0200

    Initial migration, with bug

---
+++ EfCoreNpgsqlMigrationBugPoc/Migrations/20210903163801_Initial.cs
@@ -0,0 +1,52 @@
+﻿using EfCoreNpgsqlMigrationBugPoc.First;
+using EfCoreNpgsqlMigrationBugPoc.Second;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace EfCoreNpgsqlMigrationBugPoc.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:first_some_state", "one,two")
+                .Annotation("Npgsql:Enum:second_some_state", "three,four,five");
+
+            migrationBuilder.CreateTable(
+                name: "Ones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    State = table.Column<SomeState>(type: "first_some_state", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Twos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    State = table.Column<SomeState>(type: "second_some_state", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Twos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ones");
+
+            migrationBuilder.DropTable(
+                name: "Twos");
+        }
+    }
+}
